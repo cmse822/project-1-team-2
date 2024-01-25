faa1bb9d0378a35710f524ad79eab98512249ee0
esteban <echeve11@msu.edu>
2024-01-25 11:16:59 -0500

    transpose, 5000 experiment script

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -19,6 +19,16 @@ void mat_mul(int size,vector<vector<double>> mat1, vector<vector<double>> mat2, 
     }
 }
 
+void mat_mul_transpose(int size,vector<vector<double>> mat1, vector<vector<double>> mat2, vector<vector<double>> result){
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            for(int k = 0; k < size; k++){
+                result[i][j] += mat1[i][k] * mat2[j][k];
+            }
+        }
+    }
+}
+
 void init_mat(int size, vector<vector<double>> mat) {
     // Uniform distribution [-1,1]
     random_device rd;                           // Hardware random number
@@ -46,8 +56,12 @@ void batch_experiment(int mat_size,int batch_size,ofstream& myfile){
     double sum = 0;
     double mean = 0;
     double std = 0;
+    double sum_t = 0;
+    double mean_t = 0;
+    double std_t = 0;
 
-    vector<double> times(batch_size);        
+    vector<double> times(batch_size);   
+    vector<double> times_t(batch_size);       
     for (int batch_iter = 0; batch_iter < batch_size; batch_iter++){
         // Instantiate matrices
         vector<vector<double>> A(mat_size, vector<double>(mat_size));
@@ -65,37 +79,49 @@ void batch_experiment(int mat_size,int batch_size,ofstream& myfile){
         auto end = chrono::system_clock::now();
         chrono::duration<double> time = (end - start);
 
+        auto start_t = chrono::system_clock::now();
+        mat_mul_transpose(mat_size,A,B,C);
+        auto end_t = chrono::system_clock::now();
+        chrono::duration<double> time_t = (end - start);
+
         // Calculate and store time
         sum += time.count();
         times[batch_iter] = time.count();
+        
+        sum_t += time_t.count();
+        times_t[batch_iter] = time_t.count();
     }
 
     // Calculate time average
     mean = sum/batch_size;
+    mean_t = sum_t/batch_size;
 
     // Calculate standard deviation
     sum = 0;
+    sum_t = 0;
     for (int batch_iter = 0; batch_iter < batch_size; batch_iter++){
-        sum += pow(times[batch_iter] - mean,2);
+        sum += pow(times[batch_iter] - mean,2);    
+        sum_t += pow(times_t[batch_iter] - mean_t,2);
     }
     std = sqrt(sum/batch_size);
+    std_t = sqrt(sum_t/batch_size);
 
     // Write to file
-    myfile << mat_size << "," << mean << "," << std << "," << "\n";
+    myfile << mat_size << "," << mean << "," << std << "," << mean_t << "," << std_t << "," << "\n";
     cout << mat_size << endl;
 }
 
 int main(int argc, char* argv[]){
     int num_steps = 10;
     double power_low = 1;
-    double power_high = 3;
+    double power_high = 3.7;
     double power_step = (power_high - power_low) / num_steps;
     int batch_size = 7;
 
     // Open file
     ofstream myfile;
     myfile.open ("results.csv");
-    myfile << "matrix size, average time, standard deviation" << endl;
+    myfile << "matrix size, average time, standard deviation, average time transpose, standard deviation" << endl;
     
     // Run experiment with different matrix sizes
     for (int i = 0 ; i <= num_steps; i++){
